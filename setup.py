ad260bd9bb82f3ebeb9e5c95d4b5b1ffbb3249c9
yutiansut <yutiansut@qq.com>
2019-08-21 08:28:10 +0800

    #

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
     ],
-    install_requires=['quantaxis', 'quantaxis_pubsub==1.6.1'],
+    install_requires=['quantaxis', 'quantaxis_pubsub==1.6.2'],
     entry_points={
         'console_scripts': [
             'QACTPBEE = QACTPBeeBroker.__init__:go'
